681892347967e344a05bab805f3cb02169a1c409
thpir <Thijs.Pirmez@howest.be>
2023-07-16 10:32:44 +0200

    Initial commit

---
+++ lib/data/svg_data.dart
@@ -0,0 +1,546 @@
+class SvgData {
+  List<String> toolboxList = [
+    '''
+      <svg width="200" height="120" viewBox="0 80 200 120" xmlns="http://www.w3.org/2000/svg">   
+      <path 
+          d=" 
+          M 60.5 189.5 
+          L 139.5 189.5
+          L 139.5 170.5
+          L 60.5 170.5
+          L 60.5 189.5
+          " 
+          fill="blue"
+          stroke="transparent"
+          stroke-width="1"
+      /> 
+      <path 
+          d=" 
+          M 60.5 169.5 
+          L 99.5 169.5
+          L 99.5 150.5
+          L 60.5 150.5
+          L 60.5 169.5
+          " 
+          fill="blue"
+          stroke="transparent"
+          stroke-width="1"
+      />
+      <path 
+          d=" 
+          M 100.5 169.5 
+          L 139.5 169.5
+          L 139.5 150.5
+          L 100.5 150.5
+          L 100.5 169.5
+          " 
+          fill="blue"
+          stroke="transparent"
+          stroke-width="1"
+      /> 
+      <path 
+          d=" 
+          M 60.5 149.5 
+          L 99.5 149.5
+          L 99.5 130.5
+          L 60.5 130.5
+          L 60.5 149.5
+          " 
+          fill="blue"
+          stroke="transparent"
+          stroke-width="1"
+      />
+      <path 
+          d=" 
+          M 100.5 149.5 
+          L 139.5 149.5
+          L 139.5 130.5
+          L 100.5 130.5
+          L 100.5 149.5
+          " 
+          fill="blue"
+          stroke="transparent"
+          stroke-width="1"
+      /> 
+      <line x1="61.5" y1="128.5" x2="98.5" y2="128.5" stroke="blue" stroke-width="2" stroke-linecap="round"/>
+      <line x1="101.5" y1="128.5" x2="138.5" y2="128.5" stroke="blue" stroke-width="2" stroke-linecap="round"/>
+      <line x1="101.5" y1="128.5" x2="101.5" y2="126" stroke="blue" stroke-width="2" stroke-linecap="round"/>
+      <line x1="101.5" y1="126" x2="98.5" y2="126" stroke="blue" stroke-width="2" stroke-linecap="round"/>
+
+      <line x1="65" y1="180" x2="95" y2="135" stroke="grey" stroke-width="4" stroke-linecap="round"/>
+      <line x1="135" y1="180" x2="105" y2="135" stroke="grey" stroke-width="4" stroke-linecap="round"/>
+      <line x1="65" y1="180" x2="95" y2="135" stroke="blue" stroke-width="3" stroke-linecap="round"/>
+      <line x1="135" y1="180" x2="105" y2="135" stroke="blue" stroke-width="3" stroke-linecap="round"/>
+      <line x1="65" y1="157.5" x2="80" y2="135" stroke="grey" stroke-width="4" stroke-linecap="round"/>
+      <line x1="65" y1="157.5" x2="80" y2="135" stroke="blue" stroke-width="3" stroke-linecap="round"/>
+      <line x1="135" y1="157.5" x2="120" y2="135" stroke="grey" stroke-width="4" stroke-linecap="round"/>
+      <line x1="135" y1="157.5" x2="120" y2="135" stroke="blue" stroke-width="3" stroke-linecap="round"/>
+      <line x1="80" y1="180" x2="95" y2="157.5" stroke="grey" stroke-width="4" stroke-linecap="round"/>
+      <line x1="80" y1="180" x2="95" y2="157.5" stroke="blue" stroke-width="3" stroke-linecap="round"/>
+      <line x1="120" y1="180" x2="105" y2="157.5" stroke="grey" stroke-width="4" stroke-linecap="round"/>
+      <line x1="120" y1="180" x2="105" y2="157.5" stroke="blue" stroke-width="3" stroke-linecap="round"/>
+      </svg>
+    ''',
+    '''
+      <svg width="200" height="120" viewBox="0 80 200 120" xmlns="http://www.w3.org/2000/svg">   
+      <path 
+          d=" 
+          M 60.5 189.5 
+          L 139.5 189.5
+          L 139.5 170.5
+          L 60.5 170.5
+          L 60.5 189.5
+          " 
+          fill="blue"
+          stroke="transparent"
+          stroke-width="1"
+      /> 
+
+      <g transform="translate(-6, -3)">
+      <path 
+          d=" 
+          M 60.5 169.5 
+          L 99.5 169.5
+          L 99.5 150.5
+          L 60.5 150.5
+          L 60.5 169.5
+          " 
+          fill="blue"
+          stroke="transparent"
+          stroke-width="1"
+      />
+      </g>
+
+      <g transform="translate(6, -3)">
+      <path 
+          d=" 
+          M 100.5 169.5 
+          L 139.5 169.5
+          L 139.5 150.5
+          L 100.5 150.5
+          L 100.5 169.5
+          " 
+          fill="blue"
+          stroke="transparent"
+          stroke-width="1"
+      />
+      </g>
+
+      <g transform="translate(-12, -6)">
+      <path 
+          d=" 
+          M 60.5 149.5 
+          L 99.5 149.5
+          L 99.5 130.5
+          L 60.5 130.5
+          L 60.5 149.5
+          " 
+          fill="blue"
+          stroke="transparent"
+          stroke-width="1"
+      />
+      </g>
+
+      <g transform="translate(12, -6)">
+      <path 
+          d=" 
+          M 100.5 149.5 
+          L 139.5 149.5
+          L 139.5 130.5
+          L 100.5 130.5
+          L 100.5 149.5
+          " 
+          fill="blue"
+          stroke="transparent"
+          stroke-width="1"
+      /> 
+      </g>
+
+      <g transform="translate(-12, -6)">
+      <g transform="rotate(-15, 61.5, 128.5)">
+          <line x1="61.5" y1="128.5" x2="98.5" y2="128.5" stroke="blue" stroke-width="2" stroke-linecap="round"/>
+      </g>
+      </g>
+
+      <g transform="translate(12, -6)">
+      <g transform="rotate(15, 138.5, 128.5)">
+          <line x1="101.5" y1="128.5" x2="138.5" y2="128.5" stroke="blue" stroke-width="2" stroke-linecap="round"/>
+          <line x1="101.5" y1="128.5" x2="101.5" y2="126" stroke="blue" stroke-width="2" stroke-linecap="round"/>
+          <line x1="101.5" y1="126" x2="98.5" y2="126" stroke="blue" stroke-width="2" stroke-linecap="round"/>
+      </g>
+      </g>
+
+      <g transform="rotate(-15, 65, 180)">
+          <line x1="65" y1="180" x2="95" y2="135" stroke="grey" stroke-width="4" stroke-linecap="round"/>
+          <line x1="65" y1="180" x2="95" y2="135" stroke="blue" stroke-width="3" stroke-linecap="round"/>
+      </g>
+
+      <g transform="rotate(15, 135, 180)">
+          <line x1="135" y1="180" x2="105" y2="135" stroke="grey" stroke-width="4" stroke-linecap="round"/>
+          <line x1="135" y1="180" x2="105" y2="135" stroke="blue" stroke-width="3" stroke-linecap="round"/>
+      </g>
+
+      <g transform="translate(-6, -3)">
+      <g transform="rotate(-15, 65, 157.5)">
+          <line x1="65" y1="157.5" x2="80" y2="135" stroke="grey" stroke-width="4" stroke-linecap="round"/>
+          <line x1="65" y1="157.5" x2="80" y2="135" stroke="blue" stroke-width="3" stroke-linecap="round"/>
+      </g>
+      </g>
+
+      <g transform="translate(6, -3)">
+      <g transform="rotate(15, 135, 157.5)">
+          <line x1="135" y1="157.5" x2="120" y2="135" stroke="grey" stroke-width="4" stroke-linecap="round"/>
+          <line x1="135" y1="157.5" x2="120" y2="135" stroke="blue" stroke-width="3" stroke-linecap="round"/>
+      </g>
+      </g>
+
+      <g transform="rotate(-15, 80, 180)">
+          <line x1="80" y1="180" x2="95" y2="157.5" stroke="grey" stroke-width="4" stroke-linecap="round"/>
+          <line x1="80" y1="180" x2="95" y2="157.5" stroke="blue" stroke-width="3" stroke-linecap="round"/>
+      </g>
+
+      <g transform="rotate(15, 120, 180)">
+          <line x1="120" y1="180" x2="105" y2="157.5" stroke="grey" stroke-width="4" stroke-linecap="round"/>
+          <line x1="120" y1="180" x2="105" y2="157.5" stroke="blue" stroke-width="3" stroke-linecap="round"/>
+      </g>
+      </svg>
+    ''',
+    '''
+      <svg width="200" height="120" viewBox="0 80 200 120" xmlns="http://www.w3.org/2000/svg">   
+      <path 
+          d=" 
+          M 60.5 189.5 
+          L 139.5 189.5
+          L 139.5 170.5
+          L 60.5 170.5
+          L 60.5 189.5
+          " 
+          fill="blue"
+          stroke="transparent"
+          stroke-width="1"
+      /> 
+
+      <g transform="translate(-13, -4.5)">
+      <path 
+          d=" 
+          M 60.5 169.5 
+          L 99.5 169.5
+          L 99.5 150.5
+          L 60.5 150.5
+          L 60.5 169.5
+          " 
+          fill="blue"
+          stroke="transparent"
+          stroke-width="1"
+      />
+      </g>
+
+      <g transform="translate(13, -4.5)">
+      <path 
+          d=" 
+          M 100.5 169.5 
+          L 139.5 169.5
+          L 139.5 150.5
+          L 100.5 150.5
+          L 100.5 169.5
+          " 
+          fill="blue"
+          stroke="transparent"
+          stroke-width="1"
+      />
+      </g>
+
+      <g transform="translate(-26, -9)">
+      <path 
+          d=" 
+          M 60.5 149.5 
+          L 99.5 149.5
+          L 99.5 130.5
+          L 60.5 130.5
+          L 60.5 149.5
+          " 
+          fill="blue"
+          stroke="transparent"
+          stroke-width="1"
+      />
+      </g>
+
+      <g transform="translate(26, -9)">
+      <path 
+          d=" 
+          M 100.5 149.5 
+          L 139.5 149.5
+          L 139.5 130.5
+          L 100.5 130.5
+          L 100.5 149.5
+          " 
+          fill="blue"
+          stroke="transparent"
+          stroke-width="1"
+      /> 
+      </g>
+
+      <g transform="translate(-26, -9)">
+      <g transform="rotate(-30, 61.5, 128.5)">
+          <line x1="61.5" y1="128.5" x2="98.5" y2="128.5" stroke="blue" stroke-width="2" stroke-linecap="round"/>
+      </g>
+      </g>
+
+      <g transform="translate(26, -9)">
+      <g transform="rotate(30, 138.5, 128.5)">
+          <line x1="101.5" y1="128.5" x2="138.5" y2="128.5" stroke="blue" stroke-width="2" stroke-linecap="round"/>
+          <line x1="101.5" y1="128.5" x2="101.5" y2="126" stroke="blue" stroke-width="2" stroke-linecap="round"/>
+          <line x1="101.5" y1="126" x2="98.5" y2="126" stroke="blue" stroke-width="2" stroke-linecap="round"/>
+      </g>
+      </g>
+
+      <g transform="rotate(-30, 65, 180)">
+          <line x1="65" y1="180" x2="95" y2="135" stroke="grey" stroke-width="4" stroke-linecap="round"/>
+          <line x1="65" y1="180" x2="95" y2="135" stroke="blue" stroke-width="3" stroke-linecap="round"/>
+      </g>
+
+      <g transform="rotate(30, 135, 180)">
+          <line x1="135" y1="180" x2="105" y2="135" stroke="grey" stroke-width="4" stroke-linecap="round"/>
+          <line x1="135" y1="180" x2="105" y2="135" stroke="blue" stroke-width="3" stroke-linecap="round"/>
+      </g>
+
+      <g transform="translate(-13, -4.5)">
+      <g transform="rotate(-30, 65, 157.5)">
+          <line x1="65" y1="157.5" x2="80" y2="135" stroke="grey" stroke-width="4" stroke-linecap="round"/>
+          <line x1="65" y1="157.5" x2="80" y2="135" stroke="blue" stroke-width="3" stroke-linecap="round"/>
+      </g>
+      </g>
+
+      <g transform="translate(13, -4.5)">
+      <g transform="rotate(30, 135, 157.5)">
+          <line x1="135" y1="157.5" x2="120" y2="135" stroke="grey" stroke-width="4" stroke-linecap="round"/>
+          <line x1="135" y1="157.5" x2="120" y2="135" stroke="blue" stroke-width="3" stroke-linecap="round"/>
+      </g>
+      </g>
+
+      <g transform="rotate(-30, 80, 180)">
+          <line x1="80" y1="180" x2="95" y2="157.5" stroke="grey" stroke-width="4" stroke-linecap="round"/>
+          <line x1="80" y1="180" x2="95" y2="157.5" stroke="blue" stroke-width="3" stroke-linecap="round"/>
+      </g>
+
+      <g transform="rotate(30, 120, 180)">
+          <line x1="120" y1="180" x2="105" y2="157.5" stroke="grey" stroke-width="4" stroke-linecap="round"/>
+          <line x1="120" y1="180" x2="105" y2="157.5" stroke="blue" stroke-width="3" stroke-linecap="round"/>
+      </g>
+      </svg>
+
+    ''',
+    '''
+      <svg width="200" height="120" viewBox="0 80 200 120" xmlns="http://www.w3.org/2000/svg">   
+      <path 
+          d=" 
+          M 60.5 189.5 
+          L 139.5 189.5
+          L 139.5 170.5
+          L 60.5 170.5
+          L 60.5 189.5
+          " 
+          fill="blue"
+          stroke="transparent"
+          stroke-width="1"
+      /> 
+
+      <g transform="translate(-20, -4)">
+      <path 
+          d=" 
+          M 60.5 169.5 
+          L 99.5 169.5
+          L 99.5 150.5
+          L 60.5 150.5
+          L 60.5 169.5
+          " 
+          fill="blue"
+          stroke="transparent"
+          stroke-width="1"
+      />
+      </g>
+
+      <g transform="translate(20, -4)">
+      <path 
+          d=" 
+          M 100.5 169.5 
+          L 139.5 169.5
+          L 139.5 150.5
+          L 100.5 150.5
+          L 100.5 169.5
+          " 
+          fill="blue"
+          stroke="transparent"
+          stroke-width="1"
+      />
+      </g>
+
+      <g transform="translate(-40, -8)">
+      <path 
+          d=" 
+          M 60.5 149.5 
+          L 99.5 149.5
+          L 99.5 130.5
+          L 60.5 130.5
+          L 60.5 149.5
+          " 
+          fill="blue"
+          stroke="transparent"
+          stroke-width="1"
+      />
+      </g>
+
+      <g transform="translate(40, -8)">
+      <path 
+          d=" 
+          M 100.5 149.5 
+          L 139.5 149.5
+          L 139.5 130.5
+          L 100.5 130.5
+          L 100.5 149.5
+          " 
+          fill="blue"
+          stroke="transparent"
+          stroke-width="1"
+      /> 
+      </g>
+
+      <g transform="translate(-40, -9)">
+      <g transform="rotate(-45, 61.5, 128.5)">
+          <line x1="61.5" y1="128.5" x2="98.5" y2="128.5" stroke="blue" stroke-width="2" stroke-linecap="round"/>
+      </g>
+      </g>
+
+      <g transform="translate(40, -9)">
+      <g transform="rotate(45, 138.5, 128.5)">
+          <line x1="101.5" y1="128.5" x2="138.5" y2="128.5" stroke="blue" stroke-width="2" stroke-linecap="round"/>
+          <line x1="101.5" y1="128.5" x2="101.5" y2="126" stroke="blue" stroke-width="2" stroke-linecap="round"/>
+          <line x1="101.5" y1="126" x2="98.5" y2="126" stroke="blue" stroke-width="2" stroke-linecap="round"/>
+      </g>
+      </g>
+
+      <g transform="rotate(-45, 65, 180)">
+          <line x1="65" y1="180" x2="95" y2="135" stroke="grey" stroke-width="4" stroke-linecap="round"/>
+          <line x1="65" y1="180" x2="95" y2="135" stroke="blue" stroke-width="3" stroke-linecap="round"/>
+      </g>
+
+      <g transform="rotate(45, 135, 180)">
+          <line x1="135" y1="180" x2="105" y2="135" stroke="grey" stroke-width="4" stroke-linecap="round"/>
+          <line x1="135" y1="180" x2="105" y2="135" stroke="blue" stroke-width="3" stroke-linecap="round"/>
+      </g>
+
+      <g transform="translate(-20, -4)">
+      <g transform="rotate(-45, 65, 157.5)">
+          <line x1="65" y1="157.5" x2="80" y2="135" stroke="grey" stroke-width="4" stroke-linecap="round"/>
+          <line x1="65" y1="157.5" x2="80" y2="135" stroke="blue" stroke-width="3" stroke-linecap="round"/>
+      </g>
+      </g>
+
+      <g transform="translate(20, -4)">
+      <g transform="rotate(45, 135, 157.5)">
+          <line x1="135" y1="157.5" x2="120" y2="135" stroke="grey" stroke-width="4" stroke-linecap="round"/>
+          <line x1="135" y1="157.5" x2="120" y2="135" stroke="blue" stroke-width="3" stroke-linecap="round"/>
+      </g>
+      </g>
+
+      <g transform="rotate(-45, 80, 180)">
+          <line x1="80" y1="180" x2="95" y2="157.5" stroke="grey" stroke-width="4" stroke-linecap="round"/>
+          <line x1="80" y1="180" x2="95" y2="157.5" stroke="blue" stroke-width="3" stroke-linecap="round"/>
+      </g>
+
+      <g transform="rotate(45, 120, 180)">
+          <line x1="120" y1="180" x2="105" y2="157.5" stroke="grey" stroke-width="4" stroke-linecap="round"/>
+          <line x1="120" y1="180" x2="105" y2="157.5" stroke="blue" stroke-width="3" stroke-linecap="round"/>
+      </g>
+      </svg>
+    ''',
+    '''
+      <svg width="200" height="120" viewBox="0 80 200 120" xmlns="http://www.w3.org/2000/svg">   
+      <path 
+          d=" 
+          M 60.5 189.5 
+          L 139.5 189.5
+          L 139.5 170.5
+          L 60.5 170.5
+          L 60.5 189.5
+          " 
+          fill="blue"
+          stroke="transparent"
+          stroke-width="1"
+      /> 
+      <path 
+          d=" 
+          M 30.5 169.5 
+          L 69.5 169.5
+          L 69.5 150.5
+          L 30.5 150.5
+          L 30.5 169.5
+          " 
+          fill="blue"
+          stroke="transparent"
+          stroke-width="1"
+      />
+      <path 
+          d=" 
+          M 130.5 169.5 
+          L 170.5 169.5
+          L 170.5 150.5
+          L 130.5 150.5
+          L 130.5 169.5
+          " 
+          fill="blue"
+          stroke="transparent"
+          stroke-width="1"
+      /> 
+      <path 
+          d=" 
+          M 15.5 149.5 
+          L 54.5 149.5
+          L 54.5 130.5
+          L 15.5 130.5
+          L 15.5 149.5
+          " 
+          fill="blue"
+          stroke="transparent"
+          stroke-width="1"
+      />
+      <path 
+          d=" 
+          M 145.5 149.5 
+          L 184.5 149.5
+          L 184.5 130.5
+          L 145.5 130.5
+          L 145.5 149.5
+          " 
+          fill="blue"
+          stroke="transparent"
+          stroke-width="1"
+      /> 
+
+      <g transform="rotate(-110, 16.5, 128.5)"> 
+          <line x1="16.5" y1="128.5" x2="53.5" y2="128.5" stroke="blue" stroke-width="2" stroke-linecap="round"/>
+      </g>
+      <g transform="rotate(110, 183.5, 128.5)"> 
+          <line x1="146.5" y1="128.5" x2="183.5" y2="128.5" stroke="blue" stroke-width="2" stroke-linecap="round"/>
+          <line x1="146.5" y1="128.5" x2="146.5" y2="126" stroke="blue" stroke-width="2" stroke-linecap="round"/>
+          <line x1="146.5" y1="126" x2="143.5" y2="126" stroke="blue" stroke-width="2" stroke-linecap="round"/>
+      </g>
+
+      <line x1="65" y1="180" x2="35" y2="135" stroke="grey" stroke-width="4" stroke-linecap="round"/>
+      <line x1="135" y1="180" x2="165" y2="135" stroke="grey" stroke-width="4" stroke-linecap="round"/>
+      <line x1="65" y1="180" x2="35" y2="135" stroke="blue" stroke-width="3" stroke-linecap="round"/>
+      <line x1="135" y1="180" x2="165" y2="135" stroke="blue" stroke-width="3" stroke-linecap="round"/>
+      
+      <line x1="35" y1="157.5" x2="20" y2="135" stroke="grey" stroke-width="4" stroke-linecap="round"/>
+      <line x1="35" y1="157.5" x2="20" y2="135" stroke="blue" stroke-width="3" stroke-linecap="round"/>
+      <line x1="165" y1="157.5" x2="180" y2="135" stroke="grey" stroke-width="4" stroke-linecap="round"/>
+      <line x1="165" y1="157.5" x2="180" y2="135" stroke="blue" stroke-width="3" stroke-linecap="round"/>
+      
+      <line x1="80" y1="180" x2="65" y2="157.5" stroke="grey" stroke-width="4" stroke-linecap="round"/>
+      <line x1="80" y1="180" x2="65" y2="157.5" stroke="blue" stroke-width="3" stroke-linecap="round"/>
+      <line x1="120" y1="180" x2="135" y2="157.5" stroke="grey" stroke-width="4" stroke-linecap="round"/>
+      <line x1="120" y1="180" x2="135" y2="157.5" stroke="blue" stroke-width="3" stroke-linecap="round"/>
+      </svg>
+    ''',
+  ];
+}
---
+++ lib/widgets/toolbox.dart
@@ -0,0 +1,70 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import '../data/svg_data.dart';
+
+class Toolbox extends StatefulWidget {
+  const Toolbox({super.key});
+
+  @override
+  State<Toolbox> createState() => _ToolboxState();
+}
+
+class _ToolboxState extends State<Toolbox> {
+  SvgData svgData = SvgData();
+  int currentIndex = 0;
+  bool closed = true;
+  Timer? timer;
+
+  void changeImage() {
+    setState(() {
+      if (closed) {
+        if (currentIndex < svgData.toolboxList.length - 1) {
+          currentIndex++;
+        }
+      } else {
+        if (currentIndex > 0) {
+          currentIndex--;
+        }
+      }
+    });
+  }
+
+  void startImageTimer() {
+    timer = Timer.periodic(const Duration(milliseconds: 50), (Timer t) {
+      if (closed) {
+        if (currentIndex == svgData.toolboxList.length - 1) {
+          timer?.cancel();
+          closed = false;
+        } else {
+          changeImage();
+        }
+      } else {
+        if (currentIndex == 0) {
+          timer?.cancel();
+          closed = true;
+        } else {
+          changeImage();
+        }
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      alignment: Alignment.bottomCenter,
+      child: GestureDetector(
+        onTap: startImageTimer,
+        child: SvgPicture.string(
+          svgData.toolboxList[currentIndex],
+          width: 400,
+        ),
+      ),
+    );
+  }
+}
